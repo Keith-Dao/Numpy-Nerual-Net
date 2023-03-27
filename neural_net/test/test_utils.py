3a2e5b09fafe5363917f48a72d6f11f18fa5f48e
Keith Dao <keith.dao.248@gmail.com>
2023-03-27 16:45:50 +1000

    Adds log softmax.

--- neural_net/test/test_utils.py
+++ neural_net/test/test_utils.py
@@ -4,7 +4,7 @@ This module tests the utils module.
 import numpy as np
 import pytest
 
-from neural_net.utils import softmax
+from neural_net.utils import softmax, log_softmax
 
 
 class TestSoftmax:
@@ -30,3 +30,28 @@ class TestSoftmax:
     ])
     def test_softmax(self, x, true_p):
         assert np.allclose(softmax(x), true_p, atol=1e-4)
+
+
+class TestLogSoftmax:
+    TOLERANCE = 1e-4
+
+    @pytest.mark.parametrize("x, true_p", [
+        (np.array([1, 1, 1]), np.array([-1.0986, -1.0986, -1.0986])),
+        (np.array([1, 0, 0]), np.array([-0.5514, -1.5514, -1.5514])),
+        (np.array([-1, -1, -1]), np.array([-1.0986, -1.0986, -1.0986])),
+        (np.array([999, 0, 0]), np.array([0, -999, -999])),
+        (
+            np.array([
+                [1, 1, 1],
+                [1, 0, 0],
+                [999, 0, 0]
+            ]),
+            np.array([
+                [-1.0986, -1.0986, -1.0986],
+                [-0.5514, -1.5514, -1.5514],
+                [0, -999, -999]
+            ])
+        )
+    ])
+    def test_softmax(self, x, true_p):
+        assert np.allclose(log_softmax(x), true_p, atol=1e-4)
