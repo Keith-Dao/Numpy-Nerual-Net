9dca1c83a1d12ff77b2efcca4c2ffd79d21477e6
Keith Dao <keith.dao.248@gmail.com>
2023-03-27 15:11:24 +1000

    Fixes pylint warnings.

--- neural_net/utils.py
+++ neural_net/utils.py
@@ -6,14 +6,14 @@ import numpy as np
 from numpy.typing import NDArray
 
 
-def softmax(x: NDArray) -> NDArray:
+def softmax(in_: NDArray) -> NDArray:
     """
     The softmax function.
 
     Args:
-        x: The input vector or matrix
+        in_: The input vector or matrix
     Returns:
         The row wise probability vector of the given input.
     """
-    exp = np.exp(x - np.max(x, axis=-1, keepdims=True))
+    exp = np.exp(in_ - np.max(in_, axis=-1, keepdims=True))
     return exp / exp.sum(axis=-1, keepdims=True)
